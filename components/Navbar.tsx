5aba8bc750e050f1a0580cfea4ec6326bf16d8f9
Ardian Eka Candra <ardianoption@gmail.com>
2025-01-07 18:23:56 +0700

    replace signin signout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { fontBody, fontTitle } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { AuthContext } from "./AuthProvider";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const { user, signOut } = useContext(AuthContext);
+  console.log("🚀 ~ Navbar ~ user:", user);
   return (
     <div className="bg-white shadow-lg mb-8">
       <div className="container flex justify-between py-2">
         <a href="/" className={cn(fontTitle, "text-xl text-slate-900")}>
           <h1>MyNotes</h1>
         </a>
-
         <ul className={`${fontBody} flex gap-4 flex-nowrap text-sm`}>
-          <li>
-            <Button size={"sm"} asChild>
-              <Link href={"/signin"}>Sign In</Link>
-            </Button>
-          </li>
+          {user ? (
+            <li>
+              <Button variant={"destructive"} size={"sm"}>
+                Sign Out
+              </Button>
+            </li>
+          ) : (
+            <li>
+              <Button size={"sm"} asChild>
+                <Link href={"/signin"}>Sign In</Link>
+              </Button>
+            </li>
+          )}
         </ul>
       </div>
     </div>
